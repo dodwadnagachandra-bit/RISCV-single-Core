d112cf949f23b02e1c92be591a2524ebee79650a
dodwadnagachandra-bit <dodwadnagachandra@gmail.com>
2026-04-13 20:15:48 +0530

    Updated Main_Decoder.v

--- Main_Decoder.v
+++ Main_Decoder.v
@@ -1,5 +1,5 @@
 // Main Decoder
-module mian_Decoder(op,zero,RegWrite,ResultSrc,MemWrite,AluSrc,PCSrc,ImmSrc,Aluop);
+module main_Decoder(op,zero,RegWrite,ResultSrc,MemWrite,AluSrc,PCSrc,ImmSrc,Aluop);
   
   input zero;
   input [6:0] op;
@@ -7,7 +7,8 @@ module mian_Decoder(op,zero,RegWrite,ResultSrc,MemWrite,AluSrc,PCSrc,ImmSrc,Aluo
   output [1:0] ImmSrc,Aluop;
   
   wire branch;
-  
+
+    // RegWrite: load(0000011), R-type(0110011), I-type arith(0010011)
   assign RegWrite = (( op==7'b0000011) | ( op==7'b0110011)) ? 1'b1 : 1'b0 ;
   
   assign MemWrite = (op ==7'b0100011) ? 1'b1 :1'b0;
@@ -16,7 +17,7 @@ module mian_Decoder(op,zero,RegWrite,ResultSrc,MemWrite,AluSrc,PCSrc,ImmSrc,Aluo
  
   assign AluSrc = (( op==7'b0000011) | ( op==7'b0100011)) ? 1'b1 : 1'b0 ;
   
-  assign branch = (opcode =1100011) ? 1'b1 :1'b0;
+  assign branch = (op =7'b1100011) ? 1'b1 :1'b0;
   
   assign ImmSrc = (op == 7'b0100011) ? 2'b01 :(op == 7'b1100011) ? 2'b10 : 2'b00;
   
