b16d56cf332b20309f39c62f992ec04e5293b00b
dodwadnagachandra-bit <dodwadnagachandra@gmail.com>
2026-04-13 10:28:56 +0530

    Create Sign_Extend.v

---
+++ Sign_Extend.v
@@ -0,0 +1,15 @@
+// Sign Extend 
+module Sign_Extend(in,imm_ext);
+  
+  input [31:0] in;
+  output [31:0} imm_ext;
+  
+          assign imm_ext = (in[31]) ? {{20{1'b1}},in[31:20]}:										  {{20{1'b0}},in[31:20]};
+            							
+  
+  
+  
+  
+  
+  
+endmodule
